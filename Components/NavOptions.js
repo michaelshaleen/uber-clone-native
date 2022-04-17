91d86ce06259157aae192c6b1c3c590996f74130
michaelshaleen <shaleenmichael@yahoo.com>
2022-04-17 12:42:34 -0500

    to set up live location, cannot select icons until input has been made

--- Components/NavOptions.js
+++ Components/NavOptions.js
@@ -3,6 +3,9 @@ import { TouchableOpacity, FlatList, Image, StyleSheet, Text, View } from 'react
 import tw from 'tailwind-react-native-classnames';
 import { Icon } from "react-native-elements";
 import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { selectOrigin } from '../slices/navSlice';
+
 
 
 const data = [
@@ -22,6 +25,7 @@ const NavOptions = () => {
 
 
   const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
 
 
   return (
@@ -31,10 +35,11 @@ const NavOptions = () => {
       keyExtractor={(item) => item.id}
       renderItem={({ item }) =>(
         <TouchableOpacity
-          onPress={() => navigation.navigate(item.screen)}
-          style={tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 h-52`}
+        onPress={() => navigation.navigate(item.screen)}
+        style={tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 h-52`}
+        disabled={!origin}
         >
-          <View>
+          <View style={tw `${!origin && "opacity-20 "}`}>
             <Image
             style={{width: 100, height: 100, resizeMode: "contain"}}
             source={{uri: item.image}} 

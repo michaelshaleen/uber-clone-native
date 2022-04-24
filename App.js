064a6096c54e369a802cb3f983ede309ecbb42c3
michaelshaleen <shaleenmichael@yahoo.com>
2022-04-24 13:52:39 -0500

    rides and eats icons

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, KeyboardAvoidingView, Text, View, Image, ScrollView, TextInput } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, Platform, View, Image, ScrollView, TextInput } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from "./store";
 import HomeScreen from "./screens/HomeScreen";
@@ -18,7 +18,10 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <KeyboardAvoidingView style={{flex: 1}}>
+          <KeyboardAvoidingView 
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{flex: 1}}
+            keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}>
           <Stack.Navigator>
             <Stack.Screen 
               name='Home Screen' 
--- Components/NavigateCard.js
+++ Components/NavigateCard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { REACT_APP_APIKEY } from "@env";
 import { useDispatch } from 'react-redux';
 import { setDestination, setOrigin } from '../slices/navSlice';
 import { useNavigation } from '@react-navigation/native';
+import { Icon } from "react-native-elements";
+
 import NavFavorites from '../Components/NavFavorites';
 
 
@@ -55,6 +57,22 @@ const NavigateCard = () => {
               />
             </View>
             <NavFavorites />
+        </View>
+
+        <View style={tw `flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
+          <TouchableOpacity style={tw `flex flex-row bg-black w-24 px-4 py-3 rounded-full`}>
+            <Icon name="car" type="font-awesome" color="white" size={16} />
+            <Text style={tw `text-white text-center`}> Rides</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={tw `flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
+            <Icon 
+              name="fast-food-outline" 
+              type="ionicon" color="black" 
+              size={16} 
+            />
+            <Text style={tw `text-center text-black`}> Eats</Text>
+          </TouchableOpacity>
         </View>
     </SafeAreaView>
   )

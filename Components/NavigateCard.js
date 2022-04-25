a9342ab2066db68a88eff650a3f8baee77b23012
michaelshaleen <shaleenmichael@yahoo.com>
2022-04-25 09:58:54 -0500

    car options

--- Components/NavigateCard.js
+++ Components/NavigateCard.js
@@ -60,18 +60,28 @@ const NavigateCard = () => {
         </View>
 
         <View style={tw `flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
-          <TouchableOpacity style={tw `flex flex-row bg-black w-24 px-4 py-3 rounded-full`}>
-            <Icon name="car" type="font-awesome" color="white" size={16} />
-            <Text style={tw `text-white text-center`}> Rides</Text>
+          <TouchableOpacity 
+            onPress={() => navigation.navigate('RideOptionsCard')}
+            style={tw `flex flex-row bg-black justify-between w-24 px-4 py-3 rounded-full`}>
+            <Icon 
+              name="car" 
+              type="font-awesome" 
+              color="white" 
+              size={16} 
+            />
+            <Text style={tw `text-white text-center`}>Rides</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={tw `flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
+          <TouchableOpacity 
+            style={tw `flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
             <Icon 
               name="fast-food-outline" 
               type="ionicon" color="black" 
               size={16} 
             />
-            <Text style={tw `text-center text-black`}> Eats</Text>
+            <Text style={tw `text-center text-black`}>
+              Eats
+            </Text>
           </TouchableOpacity>
         </View>
     </SafeAreaView>
--- Components/RideOptionsCard.js
+++ Components/RideOptionsCard.js
@@ -1,11 +1,71 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, TouchableOpacity, FlatList, Text, View } from 'react-native'
+import { Icon } from "react-native-elements";
+import { useNavigation } from '@react-navigation/native';
+import tw from 'tailwind-react-native-classnames';
 
+const data = [
+    {
+      id: "393",
+      icon: "Uber X",
+      multiplier: 1,
+      image: "https://links.papareact.com/3pn",
+    },
+    {
+      id: "030",
+      icon: "Uber XL",
+      multiplier: 1.2,
+      image: "https://links.papareact.com/5w8",
+    },
+    {
+      id: "422",
+      icon: "Uber LUX",
+      multiplier: 1.75,
+      image: "https://links.papareact.com/7pf",
+    },
+];
+
+// main function
 const RideOptionsCard = () => {
+
+
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>ride options</Text>
-    </View>
+    <SafeAreaView style={tw `bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity 
+          onPress={() => navigation.navigate('NavigateCard')}
+          style={tw `absolute top-3 left-5 z-50 p-3 rounded-full`}>
+          <Icon 
+            name="chevron-left"
+            type="font-awesome"
+          />
+
+
+        </TouchableOpacity>
+        
+      <Text
+        style={tw `text-center py-5 text-xl`}>
+          Select A Ride
+      </Text>
+
+      </View>
+
+      <FlatList 
+        data={data}
+        keyExtractor={(item) => item.id} 
+        renderItem={({item}) => (
+
+          <TouchableOpacity>
+            <Text>
+              Car
+            </Text>
+          </TouchableOpacity>
+        )}
+      
+      />
+      
+    </SafeAreaView>
   )
 }
 
